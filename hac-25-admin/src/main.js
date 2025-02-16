c48c21d2db9af8baef82f3becfb8936c7ec55011
St4our <stanislavd491@gmail.com>
2025-02-16 19:56:50 +0500

    only_zip
    only_zip

--- hac-25-admin/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import axios from "axios";
-import router from "./router/router";
-
-axios.defaults.baseURL = "http://213.139.211.149:5000/admin_panel";
-
-createApp(App).use(router).mount("#app");
